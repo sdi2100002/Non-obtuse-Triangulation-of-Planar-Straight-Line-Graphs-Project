f44be63d679644ec07ca214a20a4c75ce3b7a506
sdi2100002 <dimitrisanagno.io@gmail.com>
2024-10-12 20:09:14 +0300

    boost library done

--- CGAL-5.6.1/build/cmake_install.cmake
+++ CGAL-5.6.1/build/cmake_install.cmake
@@ -42,6 +42,52 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
+if(CMAKE_INSTALL_COMPONENT STREQUAL "CGAL_Qt5" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/CGAL/Qt" TYPE DIRECTORY FILES "/home/dimitris/Project1/CGAL-5.6.1/include/CGAL/Qt/")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/doc/CGAL" TYPE FILE FILES
+    "/home/dimitris/Project1/CGAL-5.6.1/AUTHORS"
+    "/home/dimitris/Project1/CGAL-5.6.1/CHANGES.md"
+    "/home/dimitris/Project1/CGAL-5.6.1/LICENSE"
+    "/home/dimitris/Project1/CGAL-5.6.1/LICENSE.GPL"
+    "/home/dimitris/Project1/CGAL-5.6.1/LICENSE.LGPL"
+    )
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE DIRECTORY FILES "/home/dimitris/Project1/CGAL-5.6.1/include/CGAL" REGEX "/\\.svn$" EXCLUDE REGEX "/Qt$" EXCLUDE)
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE PROGRAM FILES
+    "/home/dimitris/Project1/CGAL-5.6.1/scripts/cgal_create_CMakeLists"
+    "/home/dimitris/Project1/CGAL-5.6.1/scripts/cgal_create_cmake_script"
+    "/home/dimitris/Project1/CGAL-5.6.1/scripts/cgal_make_macosx_app"
+    )
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/CGAL" TYPE DIRECTORY FILES "/home/dimitris/Project1/CGAL-5.6.1/cmake/modules/")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/CGAL" TYPE FILE FILES "/home/dimitris/Project1/CGAL-5.6.1/cmake/modules/UseCGAL.cmake")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/CGAL" TYPE FILE FILES
+    "/home/dimitris/Project1/CGAL-5.6.1/lib/cmake/CGAL/CGALConfig.cmake"
+    "/home/dimitris/Project1/CGAL-5.6.1/lib/cmake/CGAL/CGALConfigBuildVersion.cmake"
+    "/home/dimitris/Project1/CGAL-5.6.1/lib/cmake/CGAL/CGALConfigVersion.cmake"
+    )
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/man/man1" TYPE FILE FILES "/home/dimitris/Project1/CGAL-5.6.1/auxiliary/cgal_create_cmake_script.1")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
---
+++ firstp.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <fstream>
+//#include "json/single_include/nlohmann/json.hpp" // Include the JSON library
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Constrained_Delaunay_triangulation_2.h>
+#include <CGAL/IO/io.h>
+#include <CGAL/Constrained_triangulation_plus_2.h>
+#include <CGAL/draw_triangulation_2.h>
+#include <cmath>
+#include <vector>
+#include <cassert>
+#include <boost/json/src.hpp>
+
+
+// #include <CGAL/draw_polygon_2.h>  // Include CGAL's drawing functions
+// #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+// #include <CGAL/Polygon_2.h>
+// #include <CGAL/Constrained_Delaunay_triangulation_2.h>
+
+using namespace boost::json;
+using namespace std;
+
+int main() {
+    object obj = {{"key", "value"}};
+    cout << obj << endl;
+    return 0;
+}
+
+// struct CDT {
+//     std::string instance_uid;
+//     int num_points;
+//     std::vector<int> points_x;
+//     std::vector<int> points_y;
+//     std::vector<int> region_boundary;
+//     int num_constraints;
+//     std::vector<std::vector<int>> additional_constraints;
+// };
+
+// // Function to load the JSON data into a CDT struct
+// void load_json_to_cdt(const std::string& file_name, CDT& cdt) {
+//     std::ifstream input_file(file_name);
+    
+//     if (!input_file.is_open()) {
+//         std::cerr << "Could not open the file!" << std::endl;
+//         return;
+//     }
+    
+//     // Parse the JSON file
+//     boost::json::value j;
+//     input_file >> j;
+    
+//     // Extract data into CDT struct
+//     cdt.instance_uid = j["instance_uid"];
+//     cdt.num_points = j["num_points"];
+//     cdt.points_x = j["points_x"].get<std::vector<int>>();
+//     cdt.points_y = j["points_y"].get<std::vector<int>>();
+//     cdt.region_boundary = j["region_boundary"].get<std::vector<int>>();
+//     cdt.num_constraints = j["num_constraints"];
+//     cdt.additional_constraints = j["additional_constraints"].get<std::vector<std::vector<int>>>();
+// }
+
+// int main() {
+//     CDT cdt;
+
+//     // Load the JSON file into the CDT struct
+//     load_json_to_cdt("input.json", cdt);
+
+//     // Output some of the loaded data to check
+//     std::cout << "Instance UID: " << cdt.instance_uid << std::endl;
+//     std::cout << "Number of Points: " << cdt.num_points << std::endl;
+//     std::cout << "First X Point: " << cdt.points_x[0] << std::endl;
+//     std::cout << "First Y Point: " << cdt.points_y[0] << std::endl;
+
+//     //CGAL::draw(cdt);
+
+//     return 0;
+// }
+
+
+
+
+
+
+// Define the kernel and the types used in the triangulation
+// typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+// typedef CGAL::Polygon_2<K> Polygon;
+// typedef CGAL::Constrained_Delaunay_triangulation_2<K> CDT;
+
+// int main() {
+//     CDT cdt;
+
+//     // Load the JSON file into the CDT struct (previous function)
+//     load_json_to_cdt("input.json", cdt);
+
+//     // Here, you would typically add vertices and constraints to the CDT
+//     for (size_t i = 0; i < cdt.num_points; ++i) {
+//         cdt.insert(Point_2(cdt.points_x[i], cdt.points_y[i]));
+//     }
+
+//     // Add constraints (edges) based on your region_boundary and additional_constraints
+//     for (const auto& constraint : cdt.additional_constraints) {
+//         cdt.insert_constraint(cdt.points_x[constraint[0]], cdt.points_y[constraint[0]],
+//                               cdt.points_x[constraint[1]], cdt.points_y[constraint[1]]);
+//     }
+
+//     // Draw the CDT
+//     CGAL::draw(cdt);  // Call CGAL's draw function
+
+//     return 0;
+// }
