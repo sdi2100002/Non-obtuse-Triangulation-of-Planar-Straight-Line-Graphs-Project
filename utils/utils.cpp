e2f8612f63cf6345817af82bd5157f939f707635
sdi2100002 <dimitrisanagno.io@gmail.com>
2024-10-24 15:26:34 +0300

    commends added to utils

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -5,7 +5,7 @@
 #include "../triangulation/triangulation.h"
 
 
-// Function to format the JSON object into the desired output format
+// This Function used to format the JSON object into the desired output format
 void printJsonFormatted(const object& obj) {
     std::cout << "{\n";
     std::cout << "  \"instance_uid\": " << obj.at("instance_uid").as_string() << ",\n";
@@ -62,10 +62,11 @@ void printJsonFormatted(const object& obj) {
     std::cout << "}\n";
 }
 
-// Load JSON file
+// This function used to load a JSON file and read its content into a string
 bool loadJsonFile(const std::string& filename, std::string& output) {
-    std::ifstream file(filename);
-    if (!file.is_open()) {
+    std::ifstream file(filename);// Open the specified file
+
+    if (!file.is_open()) { // Checking if the file was opened successfully
         std::cerr << "Could not open the file!" << std::endl;
         return false;
     }
@@ -74,57 +75,61 @@ bool loadJsonFile(const std::string& filename, std::string& output) {
     std::stringstream buffer;
     buffer << file.rdbuf();
     output = buffer.str();
-    return true;
+
+    return true; 
 }
 
-// Parse JSON input
+// This function is used to parse a JSON string input and return it as an object
 object parseJson(const std::string& input) {
     value parsed_json = parse(input);
     return parsed_json.as_object();
 }
 
-// Retrieve fields from the parsed JSON object
+// This function is used to retrieve various fields from the parsed JSON object
 void retrieveFields(const object& obj, std::string& instance_uid, int& num_points, std::vector<int>& points_x, std::vector<int>& points_y,int& num_constraints, std::vector<std::pair<int, int>>& additional_constraints,std::vector<int>& region_boundary) {
-    instance_uid = obj.at("instance_uid").as_string().c_str(); // Use at() here
-    num_points = obj.at("num_points").as_int64(); // Use at() here
+    // Retrieve instance uid and num_points as a string
+    instance_uid = obj.at("instance_uid").as_string().c_str(); 
+    num_points = obj.at("num_points").as_int64();
 
     // Retrieve points_x and points_y lists
-    for (const auto& x : obj.at("points_x").as_array()) { // Use at() here
-        points_x.push_back(x.as_int64());
+    for (const auto& x : obj.at("points_x").as_array()) { //iterate through x coordinates
+        points_x.push_back(x.as_int64());  //Add each x to the vector
     }
-    for (const auto& y : obj.at("points_y").as_array()) { // Use at() here
-        points_y.push_back(y.as_int64());
+    for (const auto& y : obj.at("points_y").as_array()) { //iterate through y coordinates
+        points_y.push_back(y.as_int64());//Add each y to the vector
     }
 
     // Retrieve num_constraints
-    num_constraints = obj.at("num_constraints").as_int64(); // Use at() here
+    num_constraints = obj.at("num_constraints").as_int64(); 
     
-    // Retrieve additional_constraints
+    // Retrieve additional_constraints as we did with points 
     for (const auto& constraint : obj.at("additional_constraints").as_array()) { // Use at() here
         auto constraint_pair = constraint.as_array();
         additional_constraints.emplace_back(constraint_pair[0].as_int64(), constraint_pair[1].as_int64());
     }
 
-    // Retrieve region_boundary
+    // Retrieve region_boundary as we did with points 
     for (const auto& boundary : obj.at("region_boundary").as_array()) { // Use at() here
         region_boundary.push_back(boundary.as_int64());
     }
 }
 
 
-// Create a vector of points from points_x and points_y
+// This function is used to create a vector of points from points_x and points_y arrays
 std::vector<std::pair<double, double>> createPointsVector(const std::vector<int>& points_x, const std::vector<int>& points_y) {
-    std::vector<std::pair<double, double>> points;
+    std::vector<std::pair<double, double>> points; //in this vector we store the result points
     for (size_t i = 0; i < points_x.size(); ++i) {
+        //Create pairs of (x,y) and add them to point vector
         points.emplace_back(static_cast<double>(points_x[i]), static_cast<double>(points_y[i]));
     }
-    return points;
+    return points;//return the vector
 }
 
-// Process triangulation
+// This function is used to process triangulation using the specified points and constraints
 void CallprocessTriangulation(const std::vector<std::pair<double, double>>& points, const std::vector<std::pair<int, int>>& additional_constraints, const std::vector<int>& region_boundary, const std::string& instance_uid) {
-    // Pass region_boundary into the CDTProcessor
+    // Create a CDTProcessor instance with the given points, constraints, boundary, and instance UID
     Triangulation::CDTProcessor cdtProcessor(points, additional_constraints, region_boundary, instance_uid);
+    // Call the main process which we are using to perform triangulation
     cdtProcessor.processTriangulation();
 }
 
--- utils/utils.h
+++ utils/utils.h
@@ -6,16 +6,22 @@
 
 using namespace boost::json;
 
+// This Function used to format the JSON object into the desired output format
 void printJsonFormatted(const object& obj);
 
+// This function used to load a JSON file and read its content into a string
 bool loadJsonFile(const std::string& filename, std::string& output);
 
+// This function is used to parse a JSON string input and return it as an object
 object parseJson(const std::string& input);
 
+// This function is used to retrieve various fields from the parsed JSON object
 void retrieveFields(const object& obj, std::string& instance_uid, int& num_points, std::vector<int>& points_x, std::vector<int>& points_y,int& num_constraints, std::vector<std::pair<int, int>>& additional_constraints,std::vector<int>& region_boundary);
 
+// This function is used to create a vector of points from points_x and points_y arrays
 std::vector<std::pair<double, double>> createPointsVector(const std::vector<int>& points_x, const std::vector<int>& points_y);
 
+// This function is used to process triangulation using the specified points and constraints
 void CallprocessTriangulation(const std::vector<std::pair<double, double>>& points, const std::vector<std::pair<int, int>>& additional_constraints, const std::vector<int>& region_boundary, const std::string& instance_uid);
 
 #endif 
