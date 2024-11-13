142611f125d7e07fb575795302da15dc58dec829
sdi2100002 <dimitrisanagno.io@gmail.com>
2024-11-13 18:31:08 +0200

    using draw in main

--- firstp.cpp
+++ firstp.cpp
@@ -1,3 +1,5 @@
+#define BOOST_ALLOW_DEPRECATED_HEADERS
+
 #include <fstream>
 #include <sstream>
 #include <vector>
@@ -5,17 +7,18 @@
 #include <QApplication>
 #include <iostream>
 #include <boost/json/src.hpp>
-#include "graphics/graphics.h"
-#include "triangulation/triangulation.h"
+#include "triangulation/triangulation.h"  // Use the typedef defined in triangulation.h
 #include "utils/utils.h"
 
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/draw_triangulation_2.h>
 
 using namespace boost::json;
 
 
 int main(int argc, char *argv[]) {
     // Create the QApplication instance
-    QApplication app(argc, argv); 
+    // QApplication app(argc, argv);
 
     // Load the JSON data from the input.json file
     std::string jsonData;
@@ -40,14 +43,29 @@ int main(int argc, char *argv[]) {
     // Retrieve necessary fields from the JSON object into the declared variables
     retrieveFields(obj, instance_uid, num_points, points_x, points_y, num_constraints, additional_constraints, region_boundary);
 
-    // Create a vector of pairs for the points
+    // Create a CGAL constrained Delaunay triangulation instance
+    CDT cdt;
+
+    // Convert points to CGAL points and add to triangulation
+    std::vector<Point> cgal_points;
+    for (int i = 0; i < num_points; ++i) {
+        cgal_points.emplace_back(points_x[i], points_y[i]);
+        cdt.insert(cgal_points.back());
+    }
+
+    // Insert constraints into the CGAL triangulation
+    for (const auto& constraint : additional_constraints) {
+        cdt.insert_constraint(cgal_points[constraint.first], cgal_points[constraint.second]);
+    }
+
+    // Use CGAL's draw function to visualize the triangulation
+    CGAL::draw(cdt);
+
+    // Create points vector from points_x and points_y if CallprocessTriangulation requires it
     auto points = createPointsVector(points_x, points_y);
-
-    // Call the visualization function for points and constraints
-    visualizePoints(points, additional_constraints);
-
-    // Process triangulation
     CallprocessTriangulation(points, additional_constraints, region_boundary, instance_uid);
 
-    return 0; 
+
+    return 0;
 }
+
